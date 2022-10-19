50474e373ad13f84e378753d62fc34515bbab391
zurielprogramador <argenis.canton08@gmail.com>
2022-10-19 10:36:09 -0500

    commit documento 3

---
+++ src/com/tuempresa/facturacion/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Cliente {
+	
+	@Id
+	@Column(length=6)
+	int numero;
+	@Column(length = 50)
+	@Required
+	String nombre;
+	
+	@Embedded
+	Direccion direccion;
+
+}
---
+++ src/com/tuempresa/facturacion/modelo/Detalle.java
@@ -0,0 +1,12 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Embeddable @Getter @Setter
+public class Detalle {
+int  cantidad;
+@ManyToOne(fetch=FetchType.LAZY,optional=true)
+Producto producto;
+}
